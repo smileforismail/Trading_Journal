904241022d3bdb9e0d447d15e19e7544e3831422
Ismail <shaikhnagismail@gmail.com>
2022-01-09 01:06:04 +0200

    set-up db file to connect to database

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors";
+
+// set-up dotenv
+dotenv.config();
+
+// connect to mongoDB
+connectToDatabase = async () => {
+	try {
+		await mongoose.connect(process.env.MONGODB);
+		console.log("\nSuccessfully connected to mongoDB\n".green.bold);
+	} catch (error) {
+		console.log("\nError connecting to mongoDB\n".red.bold);
+	}
+};
+
+export default connectToDatabase;
