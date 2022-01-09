68d225f47c0f45b80d206a5b3f6f97ca2f834e91
Ismail <shaikhnagismail@gmail.com>
2022-01-09 10:22:32 +0200

    import router file for to handle different routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 // file to connect to db
 import dbConnection from "./config/db.js";
+import router from "./routes/tradesRoute.js";
 
 // set-up express and dotenv
 dotenv.config();
@@ -14,6 +15,9 @@ dbConnection();
 app.get("/", (req, res) => {
 	res.send("Api working!");
 });
+
+// use router for all uris with api/trades/
+app.use("/api/trades", router);
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
