cf9782da943d08e8254e6c3613ad77f3b522bbda
Ismail <shaikhnagismail@gmail.com>
2022-01-09 08:48:04 +0200

    get footer to stay at the bottom with some margin at the top

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,14 +6,12 @@ import Col from "react-bootstrap/Col";
 
 const Footer = () => {
 	return (
-		<footer className="bg-dark navbar fixed-bottom">
-			<Container>
-				<Row>
-					<Col className="pt-3 text-center">
-						<p>Copyright &copy; Ismail's Trading Journal, Inc &#174;</p>
-					</Col>
-				</Row>
-			</Container>
+		<footer className="bg-dark">
+			<Row>
+				<Col className="mt-2 mb-0 text-center">
+					<p>Copyright &copy; Ismail's Trading Journal, Inc &#174;</p>
+				</Col>
+			</Row>
 		</footer>
 	);
 };
